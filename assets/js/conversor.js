7f7576f0879a182147376188dd2311f631e6a5ee
alu0100816761 <alu0100816761@ull.edu.es>
2016-03-02 18:10:56 +0000

    Construyendo la clase Medida y Temperatura

--- assets/js/conversor.js
+++ assets/js/conversor.js
@@ -1,25 +1,52 @@
 (function(exports) {
   "use strict";
 
-  function Medida(valor,tipo)  
+  function Medida(valor,tipo)
   {
     /* tipo es opcional. Debería admitir  new Medida("45.2 Km") */
     /* ademas de new Medida(45.2, "Km") */
+    var regexp = /([-+]?\d+(?:\.\d+)?)\s*([a-zA-Z]+)/;
+
+    if(valor.match(regexp)) {
+      valor = valor.match(regexp);
+      this.valor = valor[1];
+      this.tipo = valor[2];
+    } else {
+      this.valor = valor;
+      this.tipo = tipo;
+    }
   }
-  
+
   function Temperatura(valor,tipo)
   {
     /* tipo es opcional. Debería admitir new Medida("45.2 F") */
+    var regexp = /^\s*([-+]?\d+(?:\.\d*)?(?:e[-+]?\d+)?)\s*((?:f(?:a(?:r(?:e(?:n(?:h(?:e(?:i(?:t)?)?)?)?)?)?)?)?)|(?:c(?:e(?:l(?:s(?:i(?:u(?:s)?)?)?)?)?)?))\s*$/i;
+
+    if(valor.match(regexp)) {
+      valor = valor.match(regexp);
+      this.valor = valor[1];
+      this.tipo = valor[2];
+    } else {
+      this.valor = valor;
+      this.tipo = tipo;
+    }
   }
-  
+
+  Temperatura.prototype = new Medida ();
+  Temperatura.prototype.constructor = Temperatura;
+
   function Celsius(valor)
   {
+
   }
-  
+
+
+
   function Farenheit(valor)
   {
+
   }
-  
+
   exports.Temperatura = Temperatura;
   exports.Celsius = Celsius;
   exports.Farenheit = Farenheit;
@@ -29,15 +56,15 @@
         elemento  = document.getElementById('converted'),
         /* Extienda la RegeExp a la especificación. use una XRegExp */
         regexp    = /^\s*([-+]?\d+(?:\.\d+)?(?:e[+-]?\d+)?)\s*([a-z,A-Z]+)\s*$/i;
-    valor     = valor.match(regexp);
-    
+        valor     = valor.match(regexp);
+
     if (valor) {
       var numero = valor[1],
           tipo   = valor[2].toLowerCase();
-      
+
       numero = parseFloat(numero);
       console.log("Valor: " + numero + ", Tipo: " + tipo);
-      
+
       switch (tipo) {
         case 'c':
           var celsius = new Celsius(numero);
@@ -47,7 +74,7 @@
           var farenheit = new Farenheit(numero);
           elemento.innerHTML = farenheit.toCelsius().toFixed(2) + " Celsius";
           break;
-        
+
         default:
           /* rellene este código */
       }
